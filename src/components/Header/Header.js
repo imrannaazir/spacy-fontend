95d8b0988435aa5bf280ab652a4df0ece27be38a
imrannaazir <imrannaaziremon@gmail.com>
2022-05-02 11:30:26 +0600

    dash nav created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png'
 import auth from '../../firebase.init';
 import './Header.css'
 import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faGaugeHigh, faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
 
 const Header = () => {
+    const [openUser, setOpenUser] = useState(false)
     const [user, loading] = useAuthState(auth)
     const [signInWithGoogle, googleUser, googleLoading, error] = useSignInWithGoogle(auth);
 
@@ -40,16 +43,31 @@ const Header = () => {
                                 className='btn-hover border-2 px-4 py-1'>Login
                             </button>
                             :
-                            <img
-                                className=' w-14 rounded-full'
-                                src={user?.photoURL} alt="" />
+                            <div
+                                onClick={() => setOpenUser(!openUser)}
+                                className='transform  duration-500 hover:scale-75 flex items-center'>
+                                <img
+                                    className=' w-14 rounded-full'
+                                    src={user?.photoURL} alt="" />
+                                <FontAwesomeIcon className='text-xl' icon={faCaretDown} />
+                            </div>
                         }
 
                     </div>
                 </div>
 
             </div>
+            {openUser && <nav className='absolute right-0 top-[70px] bg-black border-2 rounded-md text-white p-4'>
+                <div className='w-full text-center'>
+                    <img className='w-[100px] mx-auto rounded-full' src={user?.photoURL} alt="" />
+                    <p className='text-lg font-semibold'>{user?.displayName}</p>
+                    <p className=' text-xs mb-3'>{user?.email}</p>
+                </div>
+                <hr className=' mb-8' />
+                <p><FontAwesomeIcon icon={faGaugeHigh} /> Dashboard</p>
+                <p><FontAwesomeIcon icon={faRightToBracket} /> Logout</p>
 
+            </nav>}
         </nav>
     );
 };
